9503056f392f419e088d498a6ff2403ce0793c76
Cristina0702 <81619421+Cristina0702@users.noreply.github.com>
2021-06-10 11:20:50 +0200

    Finished contact.php

--- contact.php
+++ contact.php
@@ -1,32 +1,44 @@
-<?php /*
+<?php 
     require_once __DIR__.'/bootstrap.php';
     require_once __DIR__.'/utils.php';
     require_once __DIR__.'/database.php';
-    require_once __DIR__.'/menu.php';
     require_once __DIR__.'/utils.php';
     
     //The form was submitted
     if ($_SERVER['REQUEST_METHOD'] === 'POST')
     {
         //handle form submission
-        $typeErr = $emailErr = $messageErr = "";
-        $type = $email = $message = "";
+        $typeErr = $nameErr = $emailErr = $subjectErr = $messageErr = $checkboxErr = "";
+        $qtype = $cname = $cemail = $csubject = $cmessage = "";
+        $checkbox = false;
     
-        if (!empty($_POST["type"])) {
-            $type = clean_input($_POST["type"]);
+        //validating qtype
+        if (!empty($_POST["qtype"])) {
+            $qtype = clean_input($_POST["qtype"]);
         }
         else
         {
             $typeErr = "Type is required";
             $validations['typeError'] = $typeErr;
         }
-            
-        if (!empty($_POST["email"])) {
-            $email = clean_input($_POST["email"]);
+          
+        //validating cname
+        if (!empty($_POST["cname"])) {
+            $cname = clean_input($_POST["cname"]);
+        }
+        else
+        {
+            $nameErr = "Name is required";
+            $validations['nameError'] = $nameErr;
+        }
+
+        //validating cemail
+        if (!empty($_POST["cemail"])) {
+            $cemail = clean_input($_POST["cemail"]);
             //FILTER_VALIDATE_EMAIL is one of many validation filters: https://www.php.net/manual/en/filter.filters.validate.php
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+            if (!filter_var($cemail, FILTER_VALIDATE_EMAIL))
             {
-                $emailErr= 'You did not enter a valid email.';
+                $emailErr= 'The email address entered is not valid.';
                 $validations['emailError'] = $emailErr;
             }
         }
@@ -35,11 +47,26 @@
             $emailErr = "Email is required";
             $validations['emailError'] = $emailErr;
         }
+
+        //validating csubject
+        if (!empty($_POST["csubject"])) {
+            $csubject = clean_input($_POST["csubject"]);
+            if(strlen($csubject)>100){
+                $subjectErr=  "Subject cannot be longer than 100 characters.";
+                $validations['subjectError'] = $subjectErr;
+            }
+        }
+        else
+        {
+            $subjectErr = "Subject is required";
+            $validations['subjectError'] = $subjectErr;
+        }
             
-        if (!empty($_POST["message"])) {
-            $message = clean_input($_POST["message"]);
-            if(strlen($message)>150){
-                $messageErr=  "Messages cannot be longer than 150 characters.";
+        //validating cmessage
+        if (!empty($_POST["cmessage"])) {
+            $cmessage = clean_input($_POST["cmessage"]);
+            if(strlen($cmessage)>150){
+                $messageErr=  "Messages cannot be longer than 300 characters.";
                 $validations['messageError'] = $messageErr;
             }
         }
@@ -49,35 +76,42 @@
             $validations['messageError'] = $messageErr;
         }
 
-        //If all's ok
-        if (empty($emailErr) && empty($typeErr) && empty($messageErr))
+        if(isset($_POST['checkbox'])){
+            $checkbox = clean_input($_POST['checkbox']);
+        }else{
+            //$checkbox = false;
+            $checkboxErr = "Please agree to the Privacy Policy";
+            $validations['checkboxError'] = $checkboxErr;
+        }
+
+        //If there are no errors
+        if (empty($typeErr) && empty($nameErr) && empty($emailErr) && empty($subjectErr) && empty($messageErr) && empty($checkboxErr))
         {
-             //Repopulate text fields with submitted data 
+
             $formvalues = [];
-            //Onward processing using PHPMailer using $type = $email = $message
+            //Onward processing using PHPMailer using $qtype = $cname = $cemail = $csubject = $cmessage
             $validations['pagemessage'] = "Form submitted successfully. Thank you!";
         }
         else
         {
-            //Repopulate text fields with submitted data 
-            $formvalues['email'] = $email;
-            $formvalues['message'] = $message;
+            //Repopulating the text fields with submitted data 
+            $formvalues['cname'] = $cname;
+            $formvalues['cemail'] = $cemail;
+            $formvalues['csubject'] = $csubject;
+            $formvalues['cmessage'] = $cmessage;
+            $formvalues['checkbox'] = $checkbox;
+            
             $validations['pagemessage'] = "There are some issues with this form";
         }
+            
 
         //Render view with validations
         echo $twig->render('contact.html', [ 
             'validations' => $validations, 
-            'formvalues' => $formvalues,
-            'menuTypes' => $menuTypes] );
-        
+            'formvalues' => $formvalues ]);
     }
     else
     {
         //Render view without validations
-        echo $twig->render('contact.html', ['menuTypes' => $menuTypes] );
-    }*/
-
-    require_once __DIR__. '/bootstrap.php';
- 
-    echo $twig->render('contact.html');
+        echo $twig->render('contact.html');
+    }
