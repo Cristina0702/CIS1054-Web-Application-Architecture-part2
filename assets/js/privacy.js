37d98ac586ed5c844b062dc9d18210b8e81f533a
Matthew-Amaira <81741083+Matthew-Amaira@users.noreply.github.com>
2021-06-17 22:38:11 +0200

    Delete privacy.js

--- assets/js/privacy.js
+++
@@ -1,9 +0,0 @@
-function checkPriv(){
-  alert("Went in");
-    var box = document.getElementById("checkbox")
-    if(!box.checked) {
-      alert("Please accept the Privacy Policy first");
-    }else{
-        alert("Booked");
-    }
-}
